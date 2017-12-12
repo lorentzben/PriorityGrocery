e3e7857cd300a6765666eabf6f48bdc6a8ed31e6
Ben Lorentz <blorentz@luc.edu>
2017-12-12 14:25:41 -0600

    Everything works

--- app/src/main/java/com/example/android/prioritygroceryempty/MainActivity.java
+++ app/src/main/java/com/example/android/prioritygroceryempty/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.android.prioritygroceryempty;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.net.Inet4Address;
 import java.text.DecimalFormat;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -22,10 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     PriorityQueue<Map.Entry<String,Integer>> storage = new PriorityQueue<>(1, new DescendingByPriority());
+    PriorityQueue<Map.Entry<String,Integer>> limbo = new PriorityQueue<>(1, new DescendingByPriority());
     Map<String,Double> prices = new HashMap<String, Double>();
     Double cost = 0.0;
     Double tempPrice = 0.0;
     DecimalFormat df = new DecimalFormat("$#.##");
+    final String PRIORITY_QUEUE = "prioirtyQueue";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +33,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        /*
         storage.offer(new AbstractMap.SimpleEntry<String, Integer>("butter",Integer.valueOf(3)));
         storage.offer(new AbstractMap.SimpleEntry<String, Integer>("beer",Integer.valueOf(5)));
         storage.offer(new AbstractMap.SimpleEntry<String, Integer>("cheese",Integer.valueOf(1)));
         prices.put("butter",5.5);
         prices.put("cheese", 2.22);
         prices.put("beer",3.54);
+        */
 
+        refreshView();
 
-
-        ArrayList<Map.Entry<String,Integer>> storageArrList = new ArrayList<>(storage);
-        GroceryAdapter adapter = new GroceryAdapter(this, storageArrList);
-
-        ListView listView = (ListView) findViewById(R.id.grocery_list_view);
-        listView.setAdapter(adapter);
     }
 
 
@@ -69,12 +63,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+        refreshView();
 
-        ArrayList<Map.Entry<String,Integer>> storageArrList = new ArrayList<>(storage);
-        GroceryAdapter adapter = new GroceryAdapter(this, storageArrList);
-
-        ListView listView = (ListView) findViewById(R.id.grocery_list_view);
-        listView.setAdapter(adapter);
     }
 
     public void addItem(View view) {
@@ -84,22 +74,67 @@ public class MainActivity extends AppCompatActivity {
 
         String itemName = etName.getText().toString();
         String strInt = etPriority.getText().toString();
-        Integer itemPriority = (Integer) Integer.parseInt(strInt);
-        Double itemPrice = Double.parseDouble(etPrice.getText().toString());
+        Integer itemPriority;
+        //Integer itemPriority = (Integer) Integer.parseInt(strInt);
+        Double itemPrice; //= Double.parseDouble(etPrice.getText().toString());
+        if(etName.getText().toString().equals(null)){
+            itemName = "";
+
+        }if (etPriority.getText().toString().equals(""))
+        {
+            itemPriority = 10;
+        }else{
+            itemPriority = (Integer) Integer.parseInt(strInt);
+        }
+        if (etPrice.getText().toString().equals(""))
+        {
+            itemPrice = 0.00;
+        }
+        else{
+            itemPrice = Double.parseDouble(etPrice.getText().toString());
+        }
 
         storage.offer(new AbstractMap.SimpleEntry<String, Integer>(itemName, itemPriority));
         prices.put(itemName,itemPrice);
 
-        ArrayList<Map.Entry<String,Integer>> storageArrList = new ArrayList<>(storage);
-        GroceryAdapter adapter = new GroceryAdapter(this, storageArrList);
-
-        ListView listView = (ListView) findViewById(R.id.grocery_list_view);
-        listView.setAdapter(adapter);
+        refreshView();
 
         etName.setText("");
         etPriority.setText("");
         etPrice.setText("");
 
+
+    }
+
+    public void refreshView(){
+        ArrayList<Map.Entry<String,Integer>> storageArrList = new ArrayList<>(storage);
+        GroceryAdapter adapter = new GroceryAdapter(this, storageArrList);
+
+        ListView listView = (ListView) findViewById(R.id.grocery_list_view);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+         limbo = storage;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(limbo == null){
+
+        }else{
+            storage = limbo;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        limbo = storage;
 
     }
 
